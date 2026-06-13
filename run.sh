8687d0a649437ecd66b2ff0497bb1f7e3ddf7553
Пшеничников Евгений <evgalipshenichnikov@edu.hse.ru>
2026-06-13 05:37:21 +0300

    Обновлён run.sh: добавлёны команды для создания и запуска контейнера reporter

--- run.sh
+++ run.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
 if [ "$1" = "build_generator" ]; then
-    docker build -t student-data-generator ./generator
+  docker build -t student-data-generator ./generator
 
 elif [ "$1" = "run_generator" ]; then
-    mkdir -p data
-    docker run --rm -v "$(pwd)/data:/data" student-data-generator
+  mkdir -p data
+  docker run --rm -v "$(pwd)/data:/data" student-data-generator
 
 elif [ "$1" = "create_local_data" ]; then
-    mkdir -p local_data
-    python3 generator/generate.py local_data
+  mkdir -p local_data
+  python3 generator/generate.py local_data
+
+elif [ "$1" = "build_reporter" ]; then
+  docker build -t student-data-reporter ./reporter
+
+elif [ "$1" = "run_reporter" ]; then
+  mkdir -p data
+  docker run --rm -v "$(pwd)/data:/data" student-data-reporter
 
 else
-    echo "Неизвестная команда"
-    echo "Доступные команды:"
-    echo "./run.sh build_generator"
-    echo "./run.sh run_generator"
-    echo "./run.sh create_local_data"
+  echo "Неизвестная команда"
+  echo "Доступные команды:"
+  echo "./run.sh build_generator"
+  echo "./run.sh run_generator"
+  echo "./run.sh create_local_data"
+  echo "./run.sh build_reporter"
+  echo "./run.sh run_reporter"
 fi
